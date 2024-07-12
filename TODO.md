d533e2e201919bdd98ae87397f9b725b304bfafa
NxtPerfect <alakaxan@protonmail.com>
2024-07-12 09:49:35 +0200

    Added file reading
    counts characters and sorts them in descending order

---
+++ TODO.md
@@ -0,0 +1,9 @@
+Huffman Coding
+- [/] Find probabilities of elements
+    - [x] go through string and save each character to array
+    if not in the array, and set frequency of it to 1
+    if in array, add +1 to frequency
+    the array should be storing pairs of char + frequency
+    - [x] sort the map by frequency descending
+    - [ ] create a prefix-free code to encode
+    - [ ] save new compressed file
---
+++ src/main.ex
@@ -0,0 +1,33 @@
+defmodule Compression do
+  defp readFile() do
+    file = File.read("data/test.txt")
+  end
+  defp readFile(path) do
+    file = File.read(path)
+  end
+  defp saveFile(content) do
+    File.write("data/compressed_text.txt", content)
+  end
+  defp saveFile(path, content) do
+    File.write(path, content)
+  end
+  defp compress(content) do
+    IO.puts("Yes")
+    charList = content |> String.graphemes() |> Enum.reduce(%{}, fn char, acc -> 
+      Map.put(acc, char, (acc[char] || 0) + 1)
+    end) |> Enum.sort_by(fn {_k, v} -> v end, :desc)
+    IO.puts(charList)
+  end
+  def run(filePath, compressedPath) do
+    %{size: size} = File.stat!(filePath)
+    IO.puts("#{size} bites")
+    {status, file} = readFile(filePath)
+    compressedFile = compress(file)
+    saveFile(compressedPath, compressedFile)
+    IO.puts("File compressed successfully.")
+    # %{size: size} = File.stat!(compressedPath)
+    # IO.puts("#{size} bites")
+  end
+end
+
+Compression.run("data/text.txt", "data/compressed_text.txt")
